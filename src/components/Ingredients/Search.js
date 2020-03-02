0cfcf4002055b7c2dc9d02ca525d12272a84c43b
Chris Lacey <ChrisLacey89@gmail.com>
2020-03-02 13:18:35 -0500

    clean up with useEffect

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 import Card from '../UI/Card';
@@ -7,32 +7,44 @@ import './Search.css';
 const Search = React.memo(props => {
 	const { onLoadIngredients } = props;
 	const [enteredFilter, setEnteredFilter] = useState('');
+	const inputRef = useRef();
 
 	useEffect(() => {
-		const query =
-			enteredFilter.length === 0
-				? ''
-				: `?orderBy="title"&equalTo="${enteredFilter}"`;
-		axios
-			.get(
-				'https://react-hooks-practice-8d702.firebaseio.com/ingredients.json' +
-					query
-			)
-			.then(responseData => {
-				const loadedIngredients = [];
-				const data = responseData.data;
-				console.log(data);
-				for (const key in data) {
-					loadedIngredients.push({
-						id: key,
-						title: data[key].title,
-						amount: data[key].amount
+		const timer = setTimeout(() => {
+			// enteredFilter will not be the current input, but the old one set .5 seconds ago
+			//inputRef is defined outside of closure, so it is not effected
+			if (enteredFilter === inputRef.current.value) {
+				const query =
+					enteredFilter.length === 0
+						? ''
+						: `?orderBy="title"&equalTo="${enteredFilter}"`;
+				axios
+					.get(
+						'https://react-hooks-practice-8d702.firebaseio.com/ingredients.json' +
+							query
+					)
+					.then(responseData => {
+						const loadedIngredients = [];
+						const data = responseData.data;
+						console.log(data);
+						for (const key in data) {
+							loadedIngredients.push({
+								id: key,
+								title: data[key].title,
+								amount: data[key].amount
+							});
+						}
+						onLoadIngredients(loadedIngredients);
 					});
-				}
-				onLoadIngredients(loadedIngredients);
-			});
+			}
+		}, 500);
+		//clean up effect
+		return () => {
+			clearTimeout(timer);
+		};
+
 		// will only run if entered filter or onLoadIng changes
-	}, [enteredFilter, onLoadIngredients]);
+	}, [enteredFilter, onLoadIngredients, inputRef]);
 
 	return (
 		<section className='search'>
@@ -40,6 +52,7 @@ const Search = React.memo(props => {
 				<div className='search-input'>
 					<label>Filter by Title</label>
 					<input
+						ref={inputRef}
 						type='text'
 						value={enteredFilter}
 						onChange={event => setEnteredFilter(event.target.value)}
